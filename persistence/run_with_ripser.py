9120ae8580adb828e64b749a68bf9cd8125df1d7
amker <me@marekcerny.com>
2020-04-12 12:05:04 +0200

    add all boundary

---
+++ persistence/run_with_ripser.py
@@ -0,0 +1,45 @@
+import subprocess
+import shlex
+from pathlib import Path
+
+
+def main(flags):
+    flags.output.mkdir(exist_ok=True)
+    out_path = lambda f: flags.output / f'{f.stem}.ph.txt'
+    files = list(filter(lambda f: not out_path(f).exists(), flags.files))
+    done, cnt = 0, len(files)
+    files.reverse()
+
+    while len(files) > 0:
+        ps = []
+        for _ in range(flags.threads):
+            f = files[-1]; files.pop()
+            bcomplex = f"./vcomplex {f} {flags.mode} "
+            ripser = f"./ripser/ripser --dim 5"
+            of = out_path(f).open('w')
+            p1 = subprocess.Popen(shlex.split(bcomplex), stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(shlex.split(ripser), stdin=p1.stdout, stdout=of)
+            ps.append((p2, of))
+            if len(files) == 0:
+                break
+        done += len(ps)
+        print(f'Processing {done}/{cnt}', end='\r')
+        for process, of in ps:
+            process.wait()
+            of.close()
+        
+    print(f'Processed {cnt}' + ' '*60)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--threads', '-j', type=int, default=2)
+    parser.add_argument('files', type=Path, nargs="+")
+    parser.add_argument('--mode', '-m', type=str)
+    parser.add_argument('--verbose', '-v', action="store_true")
+    parser.add_argument('--dim', type=int, default=2)
+    parser.add_argument('--output', type=Path, default=Path('out'))
+    args = parser.parse_args()
+
+    main(args)
---
+++ persistence/vcomplex.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+
+
+using space_t = vector<pair<int, vector<float>>>;
+
+template<typename T>
+bool bin_read(istream& is, T& out) {
+    return (bool) is.read(reinterpret_cast<char*>(&out), sizeof(T));
+}
+
+
+space_t parse_cifar_bin(std::string path) {
+    auto result = space_t{};
+
+    ifstream fs(path, ios::binary);
+
+    int dimension;
+    bin_read(fs, dimension);
+    // cout << dimension << endl;
+
+    int clss;
+    while (bin_read(fs, clss)) {
+        auto feat = vector<float>{};
+        feat.reserve(dimension);
+        float num;
+        for (int i = 0; i < dimension; ++i) {
+            bin_read(fs, num);
+            feat.push_back(num);
+        }
+        result.emplace_back(clss, move(feat));
+    }
+
+    return result;
+}
+
+
+float dist(const std::vector<float>& a, const std::vector<float> b) {
+    assert(a.size() == b.size());
+    float res = 0;
+    for (int i=0; i<a.size(); i++) {
+        float df = a[i]-b[i];
+        res += df*df;
+    }
+    return std::sqrt(res);
+}
+
+
+void compute(space_t& space) {
+    for (int i=0; i < space.size(); i++) {
+        auto& [clss_a, feat_a] = space[i];
+        for (int j=0; j<i; j++) {
+            auto& [clss_b, feat_b] = space[j];
+            if (clss_a == clss_b) {
+                cout << (+1.0/+0.0) << " ";
+            } else {
+                cout << dist(feat_a, feat_b) << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    std::ios_base::sync_with_stdio(false);
+    auto args = std::vector<std::string>(argv+1, argv+argc);
+    if (args.size() >= 1) {
+        auto space = parse_cifar_bin(args[0]);
+        compute(space);
+
+        return 0;
+    }
+    return 1;
+}
